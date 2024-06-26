a2078611b659772fdddb2e068126256218061a1d
boyaditya <boy.adityarr@gmail.com>
2024-06-26 14:16:18 +0700

    add questions

--- Case-1_Course 1B.py
+++ Case-1_Course 1B.py
@@ -1,3 +1,7 @@
+# • Drivers are concerned with the mileage obtained by their automobiles. One driver has kept track of several tankfuls of gasoline by recording miles driven and gallons used for each tankful. Develop a sentinel-controlled-repetition script that prompts the user to input the miles driven and gallons used for each tankful.
+# • The script should calculate and display the miles per gallon obtained for each tankful. After processing all input information, the script should calculate and display the combined miles per gallon obtained for all tankfuls (that is, total miles driven divided by total gallons used).
+
+
 def calculate_mileage():
     total_miles = 0.0
     total_gallons = 0.0
--- Case-3_Course 1B.py
+++ Case-3_Course 1B.py
@@ -1,3 +1,7 @@
+# • (COVID-19 Infection Statistics) During the first 20 days of the COVID-19 pandemic, the number of newly infected patients per day in a country were recorded.
+
+# • Place the following numbers in a list: 174, 335, 278, 214, 422, 513, 737, 672, 489, 412, 1301, 1105, 1123, 1376, 1502, 894, 665, 1704, 1656, 1342 Use the built-in functions in the statistics module to display the following statistics concerning the infection rate: minimum, maximum, range, mean, median, variance, and standard deviation.
+
 import statistics
 
 # number of infections per day
